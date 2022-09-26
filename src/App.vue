8cac185768da49a0f7693e28ad4846d33de3fcc9
Pauloszf <paulosergio.zaguini@gmail.com>
2022-09-26 17:18:00 -0400

    Fim do projeto básico

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,55 @@
     <div class="title has-text-centered">
       Todo List
     </div>
-    <div class="field is-grouped mb-5">
-      <div class="control is-expanded">
-        <input class="input" type="text" placeholder="Add a todo">
+    <form
+      @submit.prevent="addTodo"
+    >
+      <div class="field is-grouped mb-5">
+        <div class="control is-expanded">
+          <input 
+            v-model="newTodoContent"
+            class="input" 
+            type="text" 
+            placeholder="Add a todo"
+          >
+        </div>
+        <div class="control">
+          <button
+            :disabled="!newTodoContent"
+            class="button is-info"
+          >
+            Add
+          </button>
+        </div>
       </div>
-      <div class="control">
-        <a class="button is-info">
-          Add
-        </a>
-      </div>
-    </div>
+    </form>
 
-    <div v-for="i in 3" class="card mb-5">
+    <div 
+      v-for="todo in todos"
+      class="card mb-5"
+      :class="{ 'has-background-success-light' : todo.done}"
+    >
       <div class="card-content">
         <div class="content">
           <div class="columns is-mobile is-vcentered">
-            <div class="column">
-              Create a list
+            <div 
+              class="column"
+              :class="{ 'has-text-success line-through': todo.done }"
+            >
+              {{todo.content}}
             </div>
             <div class="column is-5 has-text-right">
-              <button class="button is-light">
+              <button 
+                @click="toggleDone(todo.id)"
+                class="button"
+                :class="todo.done ? 'is-success' : 'is-light'"
+              >
                 &check;
               </button>
-              <button class="button is-danger ml-2">
+              <button
+                @click="deleteTodo(todo.id)"
+                class="button is-danger ml-2"
+              >
                 &cross;
               </button>
             </div>
@@ -37,6 +63,55 @@
   </div>
 </template>
 
+<script setup>
+  /* Imports */
+  import { ref } from 'vue'
+  import { v4 as uuidv4 } from 'uuid'
+
+  /* Todos */
+
+  const todos= ref([
+    // {
+    //   id: 'id1',
+    //   content: 'Create a todo',
+    //   done: false
+    // },
+    // {
+    //   id: 'id2',
+    //   content: 'Update a todo',
+    //   done: true
+    // },
+  ])
+
+  /* Add Todo */
+  const newTodoContent = ref('');
+
+  const addTodo = () => {
+    const newTodo = {
+      id: uuidv4(),
+      content: newTodoContent.value,
+      done: false
+    }
+    
+    todos.value.unshift(newTodo)
+    newTodoContent.value='';
+  }
+
+  /* Delete todo */
+
+  const deleteTodo = id => {
+    todos.value = todos.value.filter(todo => todo.id !== id);
+  }
+
+  /* toggle done */
+const toggleDone = id => {
+  const index = todos.value.findIndex(todo => todo.id === id);
+
+  todos.value[index].done = !todos.value[index].done
+}
+
+</script>
+
 <style>
 @import 'C:/Users/paulo/Documents/Desenvolvimento/vueapplist/vueList/node_modules/bulma/css/bulma.min.css';
 
@@ -46,4 +121,7 @@
   margin: 0 auto;
 }
 
+.line-through {
+  text-decoration: line-through;
+}
 </style>
